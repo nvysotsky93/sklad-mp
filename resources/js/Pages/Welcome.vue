ad5b5e0746ed55f145a534f272e42b1cb10f86c7
nvysotsky93 <88332264+nvysotsky93@users.noreply.github.com>
2025-05-18 22:29:41 +0700

    Update Welcome.vue

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="p-4">
-    <h1 class="text-2xl font-bold">Добро пожаловать!</h1>
-    <p>Это интерфейс SkladMP</p>
+    <h1 class="text-2xl font-bold mb-4">Добро пожаловать в SkladMP</h1>
+    <p class="mb-4">Это ваш персональный интерфейс управления заказами с маркетплейсов.</p>
+    <a href="/orders" class="text-blue-500 underline">Перейти к заказам</a>
   </div>
 </template>
