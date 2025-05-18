0fd1de345c4b0e7204ba578dc2573684ee613747
nvysotsky93 <88332264+nvysotsky93@users.noreply.github.com>
2025-05-18 22:12:02 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use Inertia\Inertia;
+
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome');
 });
-
-Route::get('/home', 'HomeController@index')->name('home');
-
 
