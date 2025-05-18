862f12e65fef4d2f65a8baf1da712dd40469169c
nvysotsky93 <88332264+nvysotsky93@users.noreply.github.com>
2025-05-18 21:32:00 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\WarehouseController;
+use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\InventoryMovementController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource('warehouses', WarehouseController::class);
+Route::apiResource('products', ProductController::class);
+Route::apiResource('inventory-movements', InventoryMovementController::class);
