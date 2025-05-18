a228f9d16ecc2174f6d9aae89f7df4dfdbd6d8d9
nvysotsky93 <88332264+nvysotsky93@users.noreply.github.com>
2025-05-18 21:43:03 +0700

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
