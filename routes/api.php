a8fe5bdd583589de892f1025c9b3c1394045f7da
nvysotsky93 <88332264+nvysotsky93@users.noreply.github.com>
2025-05-18 21:41:08 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\WarehouseController;
 use App\Http\Controllers\API\ProductController;
 use App\Http\Controllers\API\InventoryMovementController;
+use App\Http\Controllers\API\AuthController;
 
-Route::apiResource('warehouses', WarehouseController::class);
-Route::apiResource('products', ProductController::class);
-Route::apiResource('inventory-movements', InventoryMovementController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('warehouses', WarehouseController::class);
+    Route::apiResource('products', ProductController::class);
+    Route::apiResource('inventory-movements', InventoryMovementController::class);
+});
