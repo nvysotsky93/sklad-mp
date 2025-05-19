e86dabd7d53599296eb877bf3d3060c199541001
nvysotsky93 <88332264+nvysotsky93@users.noreply.github.com>
2025-05-19 22:30:36 +0700

    Update Welcome.vue

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="p-4">
-    <h1 class="text-2xl font-bold mb-4">Добро пожаловать в SkladMP</h1>
-    <p class="mb-4">Это ваш персональный интерфейс управления заказами с маркетплейсов.</p>
-    <a href="/orders" class="text-blue-500 underline">Перейти к заказам</a>
-  </div>
+  <h1>Добро пожаловать в складскую систему!</h1>
 </template>
+
+<script setup>
+// setup-здесь пуст, но можно добавить логику позже
+</script>
