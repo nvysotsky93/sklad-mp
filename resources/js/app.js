b95d5cef59020c84a35202c7810316990c9abcc4
nvysotsky93 <88332264+nvysotsky93@users.noreply.github.com>
2025-05-19 23:45:37 +0700

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,12 @@
-import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/vue3'
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import { createApp, h } from 'vue';
+  import { createInertiaApp } from '@inertiajs/inertia-vue3';
+  import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-createInertiaApp({
-  resolve: name => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
-  setup({ el, App, props, plugin }) {
-    createApp({ render: () => h(App, props) })
-      .use(plugin)
-      .mount(el)
-  },
-})
+  createInertiaApp({
+    resolve: name => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+      createApp({ render: () => h(App, props) })
+        .use(plugin)
+        .mount(el);
+    },
+  });
